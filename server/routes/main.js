d77b6621cf5fa0addd57d27b463a4059e2dbc073
Abbey <emmi34.et@gmail.com>
2024-09-05 21:27:00 +0100

    Search page, Pagination and Single post page done

--- server/routes/main.js
+++ server/routes/main.js
@@ -10,24 +10,98 @@ check out how to do that.
 
 //Get Home
 router.get("/", async (request, response) => {
-    const locals = {
-        title: "techtoughts",
-        description: "Simple Blog descriptions"
-    }
-
     try {
-        const data = await Post.find()
-        response.render("index", { locals, data })
+        const locals = {
+            title: "techtoughts",
+            description: "Simple Blog descriptions"
+        }
+
+        let perPage = 5;
+        let page = request.query.page || 1;
+    
+        const data = await Post.aggregate([ { $sort: { createdAt: -1 } } ])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+    
+        // Count is deprecated - please use countDocuments
+        // const count = await Post.count();
+        const count = await Post.countDocuments({});
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+    
+        response.render('index', { 
+            locals,
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null,
+            currentRoute: '/'
+    });
+
     } catch (error) {
         console.log(error);
         
     }
+});
+
+/**
+ * GET /
+ * Post :id
+*/
+router.get('/post/:id', async (request, response) => {
+    try {
+    let slug = request.params.id;
+
+    const data = await Post.findById({ _id: slug });
+
+    const locals = {
+    title: data.title,
+    description: "Simple Blog created with NodeJs, Express & MongoDb.",
+    }
+
+    response.render('post', { 
+    locals,
+    data,
+    currentRoute: `/post/${slug}`
+    });
+} catch (error) {
+    console.log(error);
+}
 
 });
 
+/**
+ * Post /
+ * Post :id
+*/
+router.post("/search", async (request, response) => {
 
+    try {
+        const locals = {
+            title: "Search",
+            description: "Simple Blog created with NodeJs, Express & MongoDb.",
+        }
 
+        let searchTerm = request.body.searchTerm;
+        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "");
 
+    const data = await Post.find({
+        $or: [
+            { title: { $regex: new RegExp(searchNoSpecialChar, 'i') }},
+            { body: { $regex: new RegExp(searchNoSpecialChar, 'i') }}
+        ]
+        });
+
+        response.render('search', { 
+            locals,
+            data,
+            currentRoute: "/"
+            });
+    } catch (error) {
+        console.log(error);
+        
+    }
+});
 
 
 
